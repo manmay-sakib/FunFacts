a7f2834f9a5cdcd8ece8d9f8fbc4ddb964a204ab
manmay sakib <monmy.sakib@gmail.com>
2023-12-13 15:27:56 +0600

    added navigation

--- app/src/main/java/com/example/funfacts/MainActivity.kt
+++ app/src/main/java/com/example/funfacts/MainActivity.kt
@@ -10,6 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.funfacts.ui.screens.Routes
+import com.example.funfacts.ui.screens.UserInputScreen
+import com.example.funfacts.ui.screens.WelcomeScreen
 import com.example.funfacts.ui.theme.FunFactsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +23,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FunFactsTheme {
-                
+                FunFactsApp()
+
             }
         }
     }
 }
 
+@Composable
+fun FunFactsApp(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.USER_INPUT_SCREEN){
+        composable(Routes.USER_INPUT_SCREEN){
+            UserInputScreen()
+
+        }
+        composable(Routes.WELCOME_SCREEN){
+            WelcomeScreen()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/funfacts/ui/screens/UserInputScreen.kt
@@ -0,0 +1,8 @@
+package com.example.funfacts.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UserInputScreen(){
+
+}
